7124d3325f8a6b25157feddf6fafcd989983cb85
leoyolo <demonzyj56@gmail.com>
2016-07-29 10:50:17 +0800

    insignificant changes

--- mnist_path.h
+++ mnist_path.h
@@ -4,10 +4,15 @@
 #include <string>
 using std::string;
 
-const string IMAGE_TRAIN = "C:\\Users\\NTU\\data\\mnist\\train-images.idx3-ubyte";
-const string IMAGE_TEST = "C:\\Users\\NTU\\data\\mnist\\t10k-images.idx3-ubyte";
-const string LABEL_TRAIN = "C:\\Users\\NTU\\data\\mnist\\train-labels.idx1-ubyte";
-const string LABEL_TEST = "C:\\Users\\NTU\\data\\mnist\\t10k-labels.idx1-ubyte";
+const string mnist_path = "/media/leoyolo/OS/Users/NTU/data/mnist/";
+static string IMAGE_TRAIN = mnist_path + "train-images.idx3-ubyte";
+static string IMAGE_TEST  = mnist_path + "t10k-images.idx3-ubyte";
+static string LABEL_TRAIN = mnist_path + "train-labels.idx1-ubyte";
+static string LABEL_TEST  = mnist_path + "t10k-labels.idx1-ubyte";
+//const string IMAGE_TRAIN = "C:\\Users\\NTU\\data\\mnist\\train-images.idx3-ubyte";
+//const string IMAGE_TEST = "C:\\Users\\NTU\\data\\mnist\\t10k-images.idx3-ubyte";
+//const string LABEL_TRAIN = "C:\\Users\\NTU\\data\\mnist\\train-labels.idx1-ubyte";
+//const string LABEL_TEST = "C:\\Users\\NTU\\data\\mnist\\t10k-labels.idx1-ubyte";
 
 
 
